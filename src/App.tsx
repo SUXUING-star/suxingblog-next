63e85ef10de3645e1b9e63fcbf22f4e996ef0ad6
SUXUING-star <jieyalinjiu@gmail.com>
2025-05-11 12:14:14 +0800

    up

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import PostDetailPage from './pages/PostDetailPage';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import NotFoundPage from './pages/NotFoundPage';
-import SecretFilesPage from './pages/SecretFilesPage';
+
 function App() {
   return (
     <BrowserRouter>
@@ -22,7 +22,6 @@ function App() {
         {/* 独立页面 (不需要 Layout) */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dw" element={<SecretFilesPage />} /> 
 
 
         <Route path="*" element={<NotFoundPage />} />
